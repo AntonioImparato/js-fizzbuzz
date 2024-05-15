f63219e9b7de5303fcac2a94213db77bfe6097b5
AntonioImparato <antonioimparato16@gmail.com>
2024-05-15 21:15:53 +0200

    add: function

--- js/main.js
+++ js/main.js
@@ -1,2 +1,22 @@
 'use strict';
+//inizio ciclo for 
+for (let i = 1; i <= 100; i++) {
 
+    // controllo numero se divisibile per 5 e 3 
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log('FizzBuzz');
+
+    }
+    // controllo numero se divisibile per 5
+    else if (i % 5 === 0) {
+        console.log('Buzz');
+    }
+    // controllo numero se divisibile per 3 
+    else if (i % 3 === 0) {
+        console.log('Fizz');
+    }
+    // stampa il valore che non asseconda i requisiti 
+    else {
+        console.log(i);
+    }
+}
